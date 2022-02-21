d5b5b174714ae11c81921ae6f2443455a5f434e6
rRakarare <rRene333@googlemail.com>
2022-02-21 22:50:10 +0100

    articel cards

--- pages/index.js
+++ pages/index.js
@@ -2,37 +2,19 @@ import Image from 'next/image'
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import Link from 'next/link'
+import NextLink from "next/link"
+import { Link } from '@chakra-ui/react'
+import Articel_Card from '../components/Articel_Card'
 
 export default function Home({ posts }) {
   return (
     <div className="mt-5">
       {posts.map((post, index) => (
-        <Link href={'/blog/' + post.slug} passHref key={index}>
-          <div className="card mb-3 pointer" style={{ maxWidth: '540px' }}>
-            <div className="row g-0">
-              <div className="col-md-8">
-                <div className="card-body">
-                  <h5 className="card-title">{post.frontMatter.title}</h5>
-                  <p className="card-text">{post.frontMatter.description}</p>
-                  <p className="card-text">
-                    <small className="text-muted">{post.frontMatter.date}</small>
-                  </p>
-                </div>
-              </div>
-              <div className="col-md-4 m-auto">
-                <Image
-                  src={post.frontMatter.thumbnailUrl}
-                  className="img-fluid mt-1 rounded-start"
-                  alt="thumbnail"
-                  width={500}
-                  height={400}
-                  objectFit="cover"
-                />
-              </div>
-            </div>
-          </div>
-        </Link>
+        <NextLink href={'/blog/' + post.slug} passHref key={index}>
+          <Link _hover={{text_decoration: "none"}}>
+          <Articel_Card thumbnail={post.frontMatter.thumbnailUrl}/>
+          </Link>
+        </NextLink>
       ))}
     </div>
   )
@@ -57,3 +39,28 @@ export const getStaticProps = async () => {
     }
   }
 }
+
+
+{/* <div className="card mb-3 pointer" style={{ maxWidth: '540px' }}>
+            <div className="row g-0">
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">{post.frontMatter.title}</h5>
+                  <p className="card-text">{post.frontMatter.description}</p>
+                  <p className="card-text">
+                    <small className="text-muted">{post.frontMatter.date}</small>
+                  </p>
+                </div>
+              </div>
+              <div className="col-md-4 m-auto">
+                <Image
+                  src={post.frontMatter.thumbnailUrl}
+                  className="img-fluid mt-1 rounded-start"
+                  alt="thumbnail"
+                  width={500}
+                  height={400}
+                  objectFit="cover"
+                />
+              </div>
+            </div>
+          </div> */}
