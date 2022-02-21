2bfdba0f16f9a9c567e8fac955aad54ff89a13db
Rene Couturier <rRene333@googlemail.com>
2022-02-21 19:41:44 +0100

    posts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import NextNprogress from "nextjs-progressbar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+        color="black"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={4}
+      />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
