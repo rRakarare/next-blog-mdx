8c57d5c33b7fb6eb4b7544c7e1aa254c47f02305
rRakarare <rRene333@googlemail.com>
2022-03-09 07:43:43 +0100

    front

---
+++ components/HeroHome/index.js
@@ -0,0 +1,63 @@
+import * as THREE from "three";
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { ContactShadows } from "@react-three/drei";
+import Model from "./Model";
+import { Box } from "@chakra-ui/react";
+
+function Rig({ children }) {
+  const ref = useRef();
+  useFrame((state) => {
+    ref.current.rotation.y = THREE.MathUtils.lerp(
+      ref.current.rotation.y,
+      (state.mouse.x * Math.PI) / 20,
+      0.05
+    );
+    ref.current.rotation.x = THREE.MathUtils.lerp(
+      ref.current.rotation.x,
+      (state.mouse.y * Math.PI) / 20,
+      0.05
+    );
+  });
+  return <group ref={ref}>{children}</group>;
+}
+
+export default function HeroHome() {
+  return (
+    <Box
+      w={"100%"}
+      h={"100vh"}
+      style={{
+        zIndex: -10,
+        background: "linear-gradient(155deg, #ffffff 20%, #a5a3a3)",
+        marginTop: "-65px",
+      }}
+    >
+      <Canvas camera={{ position: [0, -10, 65], fov: 50 }} dpr={[1, 2]}>
+        <pointLight position={[100, 100, 100]} intensity={0.8} />
+        <hemisphereLight
+          color="#ffffff"
+          groundColor="#b9b9b9"
+          position={[-7, 25, 13]}
+          intensity={0.85}
+        />
+        <Suspense fallback={null}>
+          <group position={[0, 10, 0]}>
+            <Rig>
+              <Model url="/compressed.glb" />
+            </Rig>
+            <ContactShadows
+              rotation-x={Math.PI / 2}
+              position={[0, -35, 0]}
+              opacity={0.25}
+              width={100}
+              height={100}
+              blur={2}
+              far={50}
+            />
+          </group>
+        </Suspense>
+      </Canvas>
+    </Box>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 
 
 import { SimpleGrid, Container } from "@chakra-ui/react";
-import Hero from "../components/Hero"
-import Navbar from "../components/Navbar";
+import HeroHome from "../components/HeroHome"
+
 
 export default function Home() {
   return (
     <>
-    <Hero/>
+    <HeroHome/>
     <Container maxW='container.xl' >
       Home
     </Container>
